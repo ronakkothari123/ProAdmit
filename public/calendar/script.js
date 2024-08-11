c74a7c066402f466e1f2a3ec725c61b57d2b3b83
Ronak Kothari <rk.ronakjkothari@gmail.com>
2024-08-10 19:03:00 -0700

    Progress UI

--- public/calendar/script.js
+++ public/calendar/script.js
@@ -1,9 +1,3 @@
-let user;
-
 async function initialize(){
     await createComponent("../Components/AppSidebar.html", document.getElementById("sidebar-container"));
-
-    user = await getUser();
-
-    document.getElementById("welcome-banner").innerHTML = `Welcome ${user.fullName}`;
 } initialize();
